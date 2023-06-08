9eaeba338dae8f2161328e9851d698abb6586671
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-08 16:26:52 -0400

    coded the view and added temp beer list

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+Brews = [
+    {'name': 'Beer 1', 'type': 'Brown Ale', 'abv': 5.0 },
+    {'name': 'Beer 2', 'type': 'IPA', 'abv': 6.0 },
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def brews_index(request):
+    return render(request, 'brews/index.html', { 'brews' : brews })
