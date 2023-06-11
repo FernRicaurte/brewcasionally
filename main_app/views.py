5c3c3a1666a9a81509b9d8f3e781e34862d222cb
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-11 16:37:00 -0400

    added all relevant classes, function, and updates relating to Snack

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Brew
+from django.views.generic import ListView, DetailView
+from .models import Brew, Snack
 from .forms import EventForm
-
 
 def home(request):
     return render(request, 'home.html')
@@ -16,8 +16,22 @@ def brews_index(request):
 
 def brews_detail(request, brew_id):
     brew = Brew.objects.get(id=brew_id)
+    id_list = brew.snacks.all().values_list('id')
+    snack_brew_doesnt_have = Snack.objects.exclude(id__in=id_list)
     event_form = EventForm()
-    return render(request, 'brews/detail.html', { 'brew' : brew, 'event_form' : event_form })
+    return render(request, 'brews/detail.html', { 'brew' : brew, 'event_form' : event_form, 'snacks': snack_brew_doesnt_have })
+
+def add_event(request, brew_id):
+    form = EventForm(request.POST)
+    if form.is_valid():
+        new_event = form.save(commit=False)
+        new_event.brew_id = brew_id
+        new_event.save()
+    return redirect('detail', brew_id=brew_id)
+
+def assoc_snack(request, brew_id, snack_id):
+    Brew.objects.get(id=brew_id).snacks.add(snack_id)
+    return redirect('detail', brew_id=brew_id)
 
 class BrewCreate(CreateView):
     model = Brew
@@ -31,11 +45,22 @@ class BrewDelete(DeleteView):
     model = Brew
     success_url = '/brews'
 
-def add_event(request, brew_id):
-    form = EventForm(request.POST)
-    if form.is_valid():
-        new_event = form.save(commit=False)
-        new_event.brew_id = brew_id
-        new_event.save()
-    return redirect('detail', brew_id=brew_id)
+class SnackCreate(CreateView):
+    model = Snack
+    fields = ['name', 'description']
+
+class SnackList(ListView):
+    model = Snack
+
+class SnackDetail(DetailView):
+    model = Snack
+
+class SnackUpdate(UpdateView):
+    model = Snack
+    fields = ['name', 'description']
+
+class SnackDelete(DeleteView):
+    model = Snack
+    success_url = '/snacks/'
+
 
