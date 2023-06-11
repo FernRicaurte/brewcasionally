2cc28961e67598fe5ed9a7b7dfca4fac7bb390ce
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-11 14:56:00 -0400

    one-to-many models and modelforms added

--- main_app/models.py
+++ main_app/models.py
@@ -15,9 +15,6 @@ class Brew(models.Model):
     name = models.CharField(max_length=100)
     style = models.CharField(max_length=100)
     abv = models.CharField(max_length=100)
-
-def __str__(self):
-    return f"{self.get_occasion_display()} on {self.date}"
     
 def get_absolute_url(self):
     return reverse('detail', kwargs={'brew_id': self.id})
@@ -27,4 +24,10 @@ class Event(models.Model):
     occasion = models.CharField(max_length=1, choices=OCCASIONS, default=OCCASIONS[0][0])
 
     brew = models.ForeignKey(Brew, on_delete=models.CASCADE)
+
+def __str__(self):
+    return f"{self.get_occasion_display()} on {self.date}"
+
+class Meta:
+    ordering = ['-date']
 
