1ee87af3b71209166e002cbce45968f923d9ef55
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-10 22:44:06 -0400

    added route to show a form and create a brew in urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ path('', views.home, name='home'),
 path('about/', views.about, name='about'),
 path('brews/index', views.brews_index, name='index'),
 path('brews/<int:brew_id>/', views.brews_detail, name='detail'),
+path('brews/create/', views.BrewCreate.as_view(), name='brews_create'),
 ]
