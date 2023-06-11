c9fec5448ba97ed1560e4abbe7bb4a3ec9963a90
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-11 16:40:14 -0400

    registered snack model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 #import Brew model below
-from .models import Brew, Event
+from .models import Brew, Event, Snack
 #register model below
 admin.site.register(Brew)
 admin.site.register(Event)
+admin.site.register(Snack)
