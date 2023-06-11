c6beeb566ddf69e57261f78d66420aec859222be
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-11 00:14:55 -0400

    added two new routes: update and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ path('about/', views.about, name='about'),
 path('brews/index', views.brews_index, name='index'),
 path('brews/<int:brew_id>/', views.brews_detail, name='detail'),
 path('brews/create/', views.BrewCreate.as_view(), name='brews_create'),
+path('brews/<int:pk>/update/', views.BrewUpdate.as_view(), name='brews_update'),
+path('brews/<int:pk>/delete/', views.BrewDelete.as_view(), name='brews_delete'),
 ]
