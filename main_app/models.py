c2c7ae5b06110b670edc89551d3bb1aeacb5017e
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-11 18:27:30 -0400

    revision indents in urls

--- main_app/models.py
+++ main_app/models.py
@@ -6,39 +6,50 @@ class Snack(models.Model):
     description = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'A {self.name} {self.description}'
+        return f'A {self.description} {self.name}'
     
     def get_absolute_url(self):
         return reverse('snacks_detail', kwargs={'pk': self.id})
-
-OCCASIONS = (
-    ('S', 'Sports Viewing'),
-    ('P', 'Party'),
-    ('O', 'Outdoor Event'),
-    ('G', 'Get-Together'),
-    ('F', 'Formal Event'),
-    ('I', 'Individual Consumption'),
-    ('O', 'Other')
-)
 
 class Brew(models.Model):
     name = models.CharField(max_length=100)
     style = models.CharField(max_length=100)
     abv = models.CharField(max_length=100)
+    #M:M relationship below
     snacks = models.ManyToManyField(Snack)
     
-def get_absolute_url(self):
-    return reverse('detail', kwargs={'brew_id': self.id})
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'brew_id': self.id})
+
 
 class Event(models.Model):
+    OCCASIONS = (
+        ('S', 'Sports Viewing'),
+        ('P', 'Party'),
+        ('O', 'Outdoor Event'),
+        ('G', 'Get-Together'),
+        ('F', 'Formal Event'),
+        ('I', 'Individual Consumption'),
+        ('O', 'Other')
+    )
     date = models.DateField('event date')
     occasion = models.CharField(max_length=1, choices=OCCASIONS, default=OCCASIONS[0][0])
 
-    brew = models.ForeignKey(Brew, on_delete=models.CASCADE)
+    brew = models.ForeignKey(Brew, on_delete=models.CASCADE) 
 
 def __str__(self):
     return f"{self.get_occasion_display()} on {self.date}"
 
 class Meta:
-    ordering = ['-date']
+    ordering = ['-date'] 
+
+
+
+
+
+
 
