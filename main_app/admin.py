53d282758aec8229d3a9d157b797a88b75c5ca5f
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-11 03:07:35 -0400

    registered/imported Event in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 #import Brew model below
-from .models import Brew
+from .models import Brew, Event
 #register model below
 admin.site.register(Brew)
+admin.site.register(Event)
