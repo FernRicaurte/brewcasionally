8b8b7689aee8ea0b2056cda37f434d1e5a6f2a74
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-11 13:32:14 -0400

    added add feeding path to urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ path('brews/<int:brew_id>/', views.brews_detail, name='detail'),
 path('brews/create/', views.BrewCreate.as_view(), name='brews_create'),
 path('brews/<int:pk>/update/', views.BrewUpdate.as_view(), name='brews_update'),
 path('brews/<int:pk>/delete/', views.BrewDelete.as_view(), name='brews_delete'),
+path('brews/<int:brew_id>/add_event/', views.add_event, name='add_event'),
 ]
